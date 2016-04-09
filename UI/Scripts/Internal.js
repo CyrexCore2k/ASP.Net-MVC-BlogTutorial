47b27bf1da0706bf1a4bb5cb33bdb442cac08c6d
Spencer Ruport <spam_me_1@netortech.com>
2016-04-08 23:10:03 -0700

    Blog Phase II

---
+++ UI/Scripts/Internal.js
@@ -0,0 +1,37 @@
+﻿function __GetAppStateVariableName() {
+    return "__state";
+}
+function __URLCache(url, val) {
+
+    var urlCache = AppState().urlCache;
+
+    if (!val) {
+        var ret;
+
+        if (urlCache) {
+            for (var index = 0; index < urlCache.count; index++) {
+                if (urlCache.urls[index] == url) {
+                    ret = urlCache.cache[index];
+                    break;
+                }
+            }
+        } else {
+            ret = false;
+        }
+
+        if (ret)
+            AppState().pushMessage("Cache for URL '" + url + "' found.");
+        else
+            AppState().pushMessage("No cache for URL '" + url + "' found.");
+
+        return ret;
+    } else {
+        var index = -1;
+        $.each(urlCache.urls, function (_index, item) { if (item == url) { index = _index; return false; } });
+        if (index == -1) index = urlCache.count;
+
+        urlCache.count++;
+        urlCache.urls[index] = url;
+        urlCache.cache[index] = val;
+    }
+}
--- UI/Scripts/Util.js
+++ UI/Scripts/Util.js
@@ -1,4 +1,41 @@
-﻿function AJAXLoadData(url, data, successCallBack) {
+﻿function AppState() {
+	var dataName = __GetAppStateVariableName();
+	var state = $.data(document, dataName);
+	if(!state) {
+	    var state = {
+            pushMessage: function(msg) { this.messages.push(msg); },
+	        messages: [],
+	        urlCache: { count: 0, urls: [], cache: [] }
+	    };
+	    $.data(document, dataName, state);
+	}
+	return state;
+}
+
+function AJAXLoadHTML(url, data, successCallBack) {
+    var cache = __URLCache(url);
+    if (!cache) {
+        $.ajax({
+            type: "GET",
+            data: data,
+            url: url,
+            cache: false,
+            contentType: "application/json; charset=utf-8",
+            dataType: "html",
+            success: function (msg) {
+                __URLCache(url, msg);
+                if (successCallBack) successCallBack(msg);
+            },
+	        error: function (msg) {
+	            AppState().pushMessage("AJAXLoadHTML failed for url: " + url);
+	        }
+        });
+    } else {
+        if (successCallBack) successCallBack(cache);
+    }
+}
+
+function AJAXLoadData(url, data, successCallBack) {
     $.ajax({
         type: "POST",
         data: data,
@@ -9,7 +46,7 @@
             if (successCallBack) successCallBack(msg);
         },
         error: function (msg) {
-            alert("error.");
+            AppState().pushMessage("AJAXLoadData failed for url: " + url);
         }
     });
 }
